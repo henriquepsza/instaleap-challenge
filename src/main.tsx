5be2034e84b8fb110569c0c7e7b224b450ddf2bf
Henrique Pereira <henriquepsz@protonmail.com>
2024-08-28 13:31:15 -0300

    Configured react-query and installed react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ResponsiveLayout from "./components/ResponsiveLayout.tsx";
 import {ChakraProvider} from "@chakra-ui/react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+        },
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <ChakraProvider>
-        <ResponsiveLayout />
+          <QueryClientProvider client={queryClient}>
+              <ResponsiveLayout />
+          </QueryClientProvider>
       </ChakraProvider>
   </StrictMode>,
 )
