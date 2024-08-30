8b0bfd9e3b34ba8959ef15504e009c7d16868e0e
Henrique Pereira <henriquepsz@protonmail.com>
2024-08-30 14:17:04 -0300

    Refactor JobDetailDrawer to show more details

--- src/components/JobDetailDrawer.tsx
+++ src/components/JobDetailDrawer.tsx
@@ -7,6 +7,9 @@ import {
   DrawerOverlay,
   Box,
   Text,
+  VStack,
+  Stack,
+  Divider,
 } from '@chakra-ui/react';
 import JobResponse from '../entities/JobResponse.ts';
 
@@ -17,29 +20,111 @@ interface Props {
 }
 
 const JobDetailDrawer = ({ job, isOpen, onClose }: Props) => {
+  if (!job) return null;
+
   return (
-    <Drawer isOpen={isOpen} placement='right' onClose={onClose}>
+    <Drawer isOpen={isOpen} placement='right' onClose={onClose} size={'sm'}>
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
         <DrawerHeader>Job Details</DrawerHeader>
         <DrawerBody>
-          {job && (
+          <VStack align='start' spacing={4}>
             <Box>
-              <Text fontSize={'lg'}>Job Number: {job.job_number}</Text>
-              <Text fontSize={'md'}>Client ID: {job.client_id}</Text>
-              <Text fontSize={'md'}>
-                Start Time: {new Date(job.start_time).toLocaleString()}
-              </Text>
-              <Text fontSize={'md'}>
-                End Time: {new Date(job.end_time).toLocaleString()}
-              </Text>
-              <Text fontSize={'md'}>State: {job.state}</Text>
-              <Text fontSize={'md'}>Store: {job.store.name}</Text>
-              <Text fontSize={'md'}>Recipient: {job.recipient.name}</Text>
-              <Text fontSize={'md'}>Total Items: {job.total_items}</Text>
+              <Stack spacing={2}>
+                <Text fontSize={'lg'}>
+                  <Text as='span' fontWeight='bold'>
+                    Job Number:
+                  </Text>{' '}
+                  {job.job_number}
+                </Text>
+                <Text fontSize={'md'}>Client ID: {job.client_id}</Text>
+                <Text fontSize={'md'}>
+                  Start Time: {new Date(job.start_time).toLocaleString()}
+                </Text>
+                <Text fontSize={'md'}>
+                  End Time: {new Date(job.end_time).toLocaleString()}
+                </Text>
+                <Text fontSize={'md'}>State: {job.state}</Text>
+              </Stack>
             </Box>
-          )}
+            <Divider />
+            <Box>
+              <Stack spacing={2}>
+                <Text fontSize={'lg'} fontWeight={'bold'}>
+                  Origin
+                </Text>
+                <Text fontSize={'md'}>Name: {job.origin.name}</Text>
+                <Text fontSize={'md'}>Address: {job.origin.address}</Text>
+                {job.origin.address_two && (
+                  <Text fontSize={'md'}>
+                    Address 2: {job.origin.address_two}
+                  </Text>
+                )}
+                <Text fontSize={'md'}>City: {job.origin.city}</Text>
+                <Text fontSize={'md'}>Country: {job.origin.country}</Text>
+                {job.origin.description && (
+                  <Text fontSize={'md'}>
+                    Description: {job.origin.description}
+                  </Text>
+                )}
+                <Text fontSize={'md'}>
+                  Coordinates: {job.origin.coordinates.lat},{' '}
+                  {job.origin.coordinates.lng}
+                </Text>
+              </Stack>
+            </Box>
+            <Divider />
+            <Box>
+              <Stack spacing={2}>
+                <Text fontSize={'lg'} fontWeight={'bold'}>
+                  Destination
+                </Text>
+                <Text fontSize={'md'}>Name: {job.destination.name}</Text>
+                <Text fontSize={'md'}>Address: {job.destination.address}</Text>
+                {job.destination.address_two && (
+                  <Text fontSize={'md'}>
+                    Address 2: {job.destination.address_two}
+                  </Text>
+                )}
+                <Text fontSize={'md'}>City: {job.destination.city}</Text>
+                <Text fontSize={'md'}>Country: {job.destination.country}</Text>
+                {job.destination.description && (
+                  <Text fontSize={'md'}>
+                    Description: {job.destination.description}
+                  </Text>
+                )}
+                <Text fontSize={'md'}>
+                  Coordinates: {job.destination.coordinates.lat},{' '}
+                  {job.destination.coordinates.lng}
+                </Text>
+              </Stack>
+            </Box>
+            <Divider />
+            {job.items.length > 0 && (
+              <Box>
+                <Stack spacing={2}>
+                  <Text fontSize={'lg'} fontWeight={'bold'}>
+                    Items
+                  </Text>
+                  {job.items.map((item, index) => (
+                    <Box key={index} mb={3}>
+                      <Text fontSize={'md'}>Name: {item.name}</Text>
+                      <Text fontSize={'md'}>Quantity: {item.quantity}</Text>
+                      <Text fontSize={'md'}>
+                        Price: {item.presentation.price.amount}{' '}
+                        {item.presentation.price.currency_code}
+                      </Text>
+                      <Text fontSize={'md'}>
+                        Category: {item.attributes.category}
+                      </Text>
+                      <Text fontSize={'md'}>EAN: {item.attributes.ean}</Text>
+                    </Box>
+                  ))}
+                </Stack>
+              </Box>
+            )}
+          </VStack>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
