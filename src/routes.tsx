8e189175ef2a02271db50c06447efff0d5e9775b
Henrique Pereira <henriquepsz@protonmail.com>
2024-08-30 10:36:10 -0300

    Implemented ErrorPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import ResponsiveLayout from './components/ResponsiveLayout.tsx';
 import MainPage from './pages/MainPage.tsx';
 import AvailabilityTimeSlotsPage from './pages/AvailabilityTimeSlotsPage.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <ResponsiveLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/',
